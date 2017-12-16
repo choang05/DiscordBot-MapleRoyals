c2f924d108ccee282dfc1072207b2c2040c39e0d
Chad Hoang <chad.hoang@yahoo.com>
2017-12-16 02:45:01 -0600

    Added ability to change status using !setstatus

--- cogs/MapleRoyals.py
+++ cogs/MapleRoyals.py
@@ -3,6 +3,7 @@ import discord
 import httplib2
 import os
 import jellyfish
+from cogs.utils import checks
 from discord.ext import commands
 from apiclient import discovery
 from oauth2client import client
@@ -20,7 +21,7 @@ CONFIG_FILE_PATH = 'data/MapleRoyals/config.ini'
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 CLIENT_SECRET_FILE = 'data/MapleRoyals/client_secret.json'
-#APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 
 #   Price check source: https://docs.google.com/spreadsheets/d/1VHSCcav4Y9SDAExBYPnJbV5S8auAdChb9ylwhi_LAyQ/edit#gid=9692681
 spreadsheetSource = None
@@ -55,6 +56,14 @@ class MapleRoyals:
         self.bot = bot
         InitializeConfig()
         InitializeGoogleSheetsService()
+
+    #   Changes the status of the bot given a status message
+    @commands.command(name="setstatus", aliases=["SetStatus"])
+    @checks.is_owner()
+    async def SetStatus(self, status):
+        """Sets the status of the bot"""
+        #   change status
+        await self.bot.change_presence(game=discord.Game(name=status))
 
     @commands.group(pass_context=True, name="pc", aliases=["PriceCheck", "PC", "price", "pricecheck"])
     async def PriceCheck(self, ctx):
@@ -101,7 +110,7 @@ class MapleRoyals:
         await self.bot.say(msg)
 
     #   Price Check all scrolls
-    @PriceCheck.command(name="scrolls", aliases=["Scrolls", "scroll", "Scroll", "s", "S"], pass_context=True)
+    @PriceCheck.command(name="scrolls", aliases=["Scrolls", "scroll", "Scroll", "s", "S", "armor", "Armor", "armour", "Armour"], pass_context=True)
     async def SayScrollPrices(self, ctx):
         """Show all prices for all scrolls.
         """
@@ -741,38 +750,7 @@ def InitializeGoogleSheetsService():
     global SheetsService
     #   Initialize Google API credentials
     print ("Initializing Google sheets services...")
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?''version=v4')
-    SheetsService = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
-
-#   Returns credentials for Google API
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    SheetsService = discovery.build('sheets', 'v4',  developerKey='AIzaSyD81_C0xYmRHR5bHqZxwtWVhJe4vQQXw9Y')
 
 #   Parse the config file and evaluate
 def InitializeConfig():
